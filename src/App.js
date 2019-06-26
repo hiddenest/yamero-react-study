40f59bf1a2c0c169ce146cdce2453527e0ab5a0a
Chanhee Lee <chanhee@ab180.co>
2019-06-26 16:15:36 +0900

    [LECTURE-2] 2차시 학습 결과 업데이트
    - 드롭다운 목록을 제대로 그리는 방법 학습
    - 드롭다운 목록 내의 아이템을 누르면, 선택한 값을 바꿔줌
    - 목록 바깥 누르면 알아서 드롭다운 닫히게 하기
    - 드롭다운의 컴포넌트화!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+
+import Dropdown from './Dropdown';
+
 import './App.css';
 
 import './Button.css';
 import './Dropdown.css';
 
 function App() {
-  const [blink, setBlink] = useState(false);
-  const [list] = useState(['ㅁㄴㅇㄹ', 4, '안녕하세요', '으아아', '띠용']);
+  const [a, setA] = useState();
+  const [b, setB] = useState();
 
-  function handleClickButton() {
-    setBlink(!blink);
+  function handleClickItemA(item) {
+    setA(item);
   }
+
+  function handleClickItemB(item) {
+    setB(item);
+  }
+
+  // 값이 짠짜잔 바뀜
+  console.error('testing', a, b);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className='dropdown'>
-          <button
-            className='btn btn-sub'
-            onClick={handleClickButton}
-          >
-            짜잔
-          </button>
-          {blink === true && (
-            <ul className='menu-md'>
-              {list.map((item) => (
-                <li>
-                  <button>
-                    {item}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+        <Dropdown
+          list={[1, '가나다', '찬희', 'asdf']}
+          onChange={handleClickItemA}
+        />
+        <Dropdown
+          list={[3,4,5,6,7]}
+          onChange={handleClickItemB}
+        />
       </header>
     </div>
   );
---
+++ src/Dropdown.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import './Button.css';
+import './Dropdown.css';
+
+function Dropdown({ list, onChange }) {
+  const dropdown = useRef();
+  const [blink, setBlink] = useState(false);
+  const [selectedItem, setSelectedItem] = useState();
+
+  function handleClickButton(event) {
+    setBlink(!blink);
+  }
+
+  function handleClickItem(item) {
+    setSelectedItem(item); // 선택한 값 업데이트
+    setBlink(false); // 목록 닫기
+    onChange(item); // 바꾸고자 하는 값을 위에 넘겨주기
+  }
+
+  function handleClickDocument(event) {
+    // 현재의(current) 드롭다운(dropdown)이 이벤트가 일어난 위치(event.target) 위에 있니?
+    if (!dropdown.current.contains(event.target)) {
+      setBlink(false);
+    }
+  }
+
+  // 1. 상태(데이터) 업데이트되었을 때 = effect
+  // 2. 컴포넌트가 생겨났을 때 = effect
+  // 3. 컴포넌트가 사라지기 직전 = effect
+  useEffect(() => {
+    // 컴포넌트가 생겨났을 때, 상태(데이터) 업데이트되었을 때
+    document.addEventListener('click', handleClickDocument);
+
+    // 컴포넌트가 사라지기 직전
+    return () => {
+      document.removeEventListener('click', handleClickDocument);
+    };
+  });
+
+  return (
+    <div ref={dropdown} className='dropdown'>
+      <button
+        className='btn btn-sub'
+        onClick={handleClickButton}
+      >
+        {selectedItem || '짜잔'}
+      </button>
+      {blink === true && (
+        <ul className='menu-md'>
+          {list.map((item) => (
+            <li key={item}>
+              <button
+                className={selectedItem === item ? 'on' : ''}
+                onClick={() => handleClickItem(item)}
+              >
+                {item}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
